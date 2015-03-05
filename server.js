67b3a28209091bb2067963fc1396d72ec6e6d576
Alex Millward <millwarda@googlemail.com>
2015-03-05 15:10:09 +0000

    Finished static serving

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 var http = require('http');
+var url = require('url');
+var static = require('node-static');
+
+var file = static.Server('./public');
+
+var server = http.createServer(function(request, response) {
+  var path = url.parse(request.url).pathname;
+  if (path == '/') {
+    // Index static
+    response.writeHead(302, {
+      'Location': '/index.html'
+    });
+    response.end();
+  } else if (path.slice(0, 4) == '/api') {
+    // API
+
+    response.end();
+  } else {
+    // General static
+
+    response.end();
+  }
+});
+
+server.listen(3000);
