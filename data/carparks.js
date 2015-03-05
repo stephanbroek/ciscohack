64b6f3ae6719946bd8ff4067e3973fe785f97361
Alex Millward <millwarda@googlemail.com>
2015-03-05 21:43:23 +0000

    Enhanced carparks

--- data/carparks.js
+++ data/carparks.js
@@ -1,45 +1,157 @@
 module.exports = {
   'Aquatic Centre': {
-    Id: '21905',
-    State: 'Full',
-    LastUpdated: '2015-02-18T12:20:23',
-    Latitude: 53.4687625042708,
-    Longitude: -2.23232249208155,
-    SCN: 'C09004',
-    Capacity: 984,
-    SpacesNow: 0,
-    PredictedSpaces30Mins: 9,
-    PredictedSpaces60Mins: 17
+    latitude: 53.4687625042708,
+    longitude: -2.23232249208155,
+    capacity: 984
   },
   'MEN Arena': {
-    Id: "21906",
-    State: "Spaces",
-    LastUpdated: "2015-02-18T12:20:22",
-    Latitude: 53.4888583318137,
-    Longitude: -2.2422579081768,
-    SCN: "C01001",
-    Capacity: 936,
-    SpacesNow: 179,
-    PredictedSpaces30Mins: 203,
-    PredictedSpaces60Mins: 226
+    latitude: 53.4888583318137,
+    longitude: -2.2422579081768,
+    capacity: 936
   },
-  'Manchester Oxford Road': {
-    capacity: 5211,
-    threshold: 4800,
-    latitude: 53.47404,
-    longitude: -2.24199
+  'High St. (Arndale)': {
+    latitude: 53.4841543376328,
+    longitude: -2.23933788859118,
+    capacity: 1487
   },
-  'Stockport': {
-    capacity: 3088,
-    threshold: 2800,
-    latitude: 53.408619,
-    longitude: -2.162632
+  'Bridgewater': {
+    latitude: 53.473213550616,
+    longitude: -2.24932586696902,
+    capacity: 288
   },
-  'Bolton': {
-    capacity: 2771,
-    threshold: 2500,
-    latitude: 53.577325,
-    longitude: -2.432879
+  'Charles Street': {
+    latitude: 53.4745708807856,
+    longitude: -2.23584925787353,
+    capacity: 850
+  },
+  'Chester Street': {
+    latitude: 53.4725481576253,
+    longitude: -2.24061390951779,
+    capacity: 215
+  },
+  'Chorlton Street': {
+    latitude: 53.4780194287306,
+    longitude: -2.23728449276931,
+    capacity: 559
+  },
+  'Church Street': {
+    latitude: 53.4831622030376,
+    longitude:-2.23654457792007,
+    capacity: 699
+  },
+  'Dale Street': {
+    latitude: 53.4800709866657,
+    longitude: -2.23155505280234,
+    capacity: 290
+  },
+  'Exchange Station': {
+    latitude: 53.4863694858423,
+    longitude: -2.24619211636007,
+    capacity: 494
+  },
+  'Faulkner Street': {
+    latitude: 53.4786879950399,
+    longitude: -2.24007567346395,
+    capacity: 76
+  },
+  'Gmex (lower)': {
+    latitude: 53.476067951511,
+    longitude: -2.24679604003227,
+    capacity: 719
+  },
+  'Gmex (upper)': {
+    latitude: 53.4747225339945,
+    longitude: -2.24981674488865,
+    capacity: 149
+  },
+  'Great Northern (Watson Street)': {
+    latitude: 53.4767278455969,
+    longitude: -2.24934622751422,
+    capacity: 519
+  },
+  'Great Northern B': {
+    latitude: 53.4775011200017,
+    longitude: -2.24920003046526,
+    capacity: 719
+  },
+  'King Street': {
+    latitude: 53.4817979700919,
+    longitude: -2.24890844177046,
+    capacity: 543
+  },
+  'Marks & Spencer': {
+    latitude: 53.4843045105675,
+    longitude: -2.24508019738188,
+    capacity: 150
+  },
+  'Major Street': {
+    latitude: 53.4790117261535,
+    longitude: -2.2354668452755,
+    capacity: 134
+  },
+  'Market Place': {
+    latitude: 53.4839501472679,
+    longitude: -2.24694678408294,
+    capacity: 328
+  },
+  'Oxford Street': {
+    latitude: 53.4756804052158,
+    longitude: -2.2428763730202,
+    capacity: 440
+  },
+  'Palace': {
+    latitude: 53.4755076052951,
+    longitude: -2.23939498952022,
+    capacity: 407
+  },
+  'Piazza': {
+    latitude: 53.4776250476636,
+    longitude: -2.24124494241048,
+    capacity: 225
+  },
+  'Piccadilly Place': {
+    latitude: 53.4775874019409,
+    longitude: -2.23300307464711,
+    capacity: 527
+  },
+  'Piccadilly Plaza': {
+    latitude: 53.4797255129328,
+    longitude: -2.23813777192966,
+    capacity: 169
+  },
+  'Piccadilly Station': {
+    latitude: 53.4773587769078,
+    longitude: -2.2303801884827,
+    capacity: 857
+  },
+  'Printworks': {
+    latitude: 53.4855492840776,
+    longitude: -2.23845659849906,
+    capacity: 751
+  },
+  'Sackville Street': {
+    latitude: 53.4771811649995,
+    longitude: -2.23848521185694,
+    capacity: 120
+  },
+  'Spinning Fields': {
+    latitude: 53.4799617305102,
+    longitude: -2.25451835442381,
+    capacity: 680
+  },
+  'Stanley Street': {
+    latitude: 53.4807554447985,
+    longitude: -2.25744637743922,
+    capacity: 413
+  },
+  'Tariff Street': {
+    latitude: 53.4815746476107,
+    longitude: -2.23020706652795,
+    capacity: 242
+  },
+  'The Grand (Piccadilly)': {
+    latitude: 53.4801916659909,
+    longitude: -2.23420765547439,
+    capacity: 701
   }
-
 };
