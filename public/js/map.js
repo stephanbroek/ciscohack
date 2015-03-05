ff16e5864eb84025fad15fa458dc377d240a2edb
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2015-03-05 16:07:21 +0000

    sorted inline script tag issue

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,72 @@
 $(document).ready(function() {
 
-                console.log("hello");
+    //Read in csv data file
+    
+    $.ajax({
+        type: "GET",
+        url: "data/linkDescriptions.csv",
+        dataType: "text",
+        success: function(data) {displayPoints(data);}
+     });
 
-                // create a map in the "map" div, set the view to a given place and zoom
 
-                var map = L.map('map').setView([51.505, -0.09], 13);
-
-                // add an OpenStreetMap tile layer
-                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
-
-                // add a marker in the given location, attach some popup content to it and open the popup
-                L.marker([51.5, -0.09]).addTo(map)
-                    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-                    .openPopup();
 
 });
+
+function displayPoints(csv) {
+
+    //convert csv to json
+    var lines=csv.split("\n");
+ 
+    var result = [];
+
+    var headers=lines[0].split(",");
+
+    for(var i=1;i<lines.length;i++){
+
+      var obj = {};
+      var currentline=lines[i].split(",");
+
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+
+    }
+
+    //result is now an array of jsons
+    console.log(result);
+
+    //Create a map
+
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+
+
+    //for each result in the array, add a start lat and start lon field then plot it on the map
+    for (var i=0; i<result.length; i++) {
+
+        //Convert os grid reference to lat lon
+        var osGridStart= result[i].StartGrid;
+        var osGridEnd= result[i].EndGrid;
+        var osReferenceStart = new OSRef(osGridStart.substring(0,6), osGridStart.substring(6,12));
+        var osReferenceEnd = new OSRef(osGridEnd.substring(0,6), osGridEnd.substring(6,12));
+
+        var latLonObjectStart = osReferenceStart.toLatLng();
+        var latLonObjectEnd = osReferenceEnd.toLatLng();
+        
+
+        L.marker([latLonObjectStart.lat,latLonObjectStart.lng]).addTo(map)
+        L.marker([latLonObjectEnd.lat,latLonObjectEnd.lng]).addTo(map)
+    }
+
+    //for each result in the array,
+    //plot the points on a leaflet map
+
+}
--- public/js/views/MapView.js
+++ public/js/views/MapView.js
@@ -8,10 +8,11 @@ define([
     'L'
 ], function($, _, Backbone, MapTemplate,jscoord,L) {
 
-
+    var that;
     var MapView = Backbone.View.extend({
 
         initialize: function(options) {
+            that = this;
 
         },
         render: function() {
@@ -19,8 +20,79 @@ define([
             var template = _.template(MapTemplate);
             
             this.$el.html(template);
+            this.getData();
+
+        },
+
+        getData : function() {
+
+            $.ajax({
+                type: "GET",
+                url: "data/linkDescriptions.csv",
+                dataType: "text",
+                success: function(data) {that.displayMap(data);}
+            });
+
+        },
+
+        displayMap : function(csv) {
+             //convert csv to json
+            var lines=csv.split("\n");
+         
+            var result = [];
+
+            var headers=lines[0].split(",");
+
+            for(var i=1;i<lines.length;i++){
+
+              var obj = {};
+              var currentline=lines[i].split(",");
+
+              for(var j=0;j<headers.length;j++){
+                  obj[headers[j]] = currentline[j];
+              }
+
+              result.push(obj);
+
+            }
+
+            //result is now an array of jsons
+            console.log(result);
+
+            //Create a map
+
+            var map = L.map(this.$('#map')[0]).setView([51.505, -0.09], 13);
+
+            // add an OpenStreetMap tile layer
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+
+
+            //for each result in the array, add a start lat and start lon field then plot it on the map
+            for (var i=0; i<result.length; i++) {
+
+                //Convert os grid reference to lat lon
+                var osGridStart= result[i].StartGrid;
+                var osGridEnd= result[i].EndGrid;
+                var osReferenceStart = new OSRef(osGridStart.substring(0,6), osGridStart.substring(6,12));
+                var osReferenceEnd = new OSRef(osGridEnd.substring(0,6), osGridEnd.substring(6,12));
+
+                var latLonObjectStart = osReferenceStart.toLatLng();
+                var latLonObjectEnd = osReferenceEnd.toLatLng();
+                
+
+                L.marker([latLonObjectStart.lat,latLonObjectStart.lng]).addTo(map)
+                L.marker([latLonObjectEnd.lat,latLonObjectEnd.lng]).addTo(map)
+            }
+
+            //for each result in the array,
+            //plot the points on a leaflet map
 
         }
+
+
     });
 
     return MapView;
