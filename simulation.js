25baea1b4d21af07c8ba61537e24fa31a5e2df16
Alex Millward <millwarda@googlemail.com>
2015-03-06 01:33:34 +0000

    Added delay to bolton + stockport

--- simulation.js
+++ simulation.js
@@ -50,7 +50,8 @@ function getHigher(t) {
   }
 }
 
-function getProportion(t) {
+function getProportion(ti, delay) {
+  var t = (ti - delay) % 96;
   var io = k[0].indexOf(t);
   if (io > -1)
     return k[1][io];
@@ -63,12 +64,12 @@ function getProportion(t) {
 }
 
 function getPeople(name) {
-  return Math.floor((stationList[name].max_people) * bias(proportion, 0.05));
+  return Math.floor((stationList[name].max_people) * bias(getProportion(time, stationList[name].delay), 0.05));
 
 }
 
 function getSpaces(name) {
-  return carparkList[name].capacity - Math.floor(carparkList[name].capacity * proportion);
+  return carparkList[name].capacity - Math.floor(carparkList[name].capacity * getProportion(time, 0));
 }
 
 function getStations() {
@@ -110,7 +111,6 @@ function getRoadUsage(callback) {
 
 module.exports = {
   getData: function(callback) {
-    proportion = getProportion(time);
     time = (time + 1) % 96;
     getRoadUsage(callback);
   },
