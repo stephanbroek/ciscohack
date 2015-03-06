e765073eaa241716e4a86706b140b6c780d3b8ca
Alex Millward <millwarda@googlemail.com>
2015-03-06 00:43:38 +0000

    Fixed Maybe

--- data/stations.js
+++ data/stations.js
@@ -1,30 +1,30 @@
 module.exports = {
   'ManchesterPicadilly': {
-    people: 22025,
+    max_people: 22025,
     threshold: 19000,
     latitude: 53.47738,
     longitude: -2.23091
   },
   'ManchesterVictoria': {
-    people: 5839,
+    max_people: 5839,
     threshold: 5000,
     latitude: 53.48748,
     longitude: -2.2426
   },
   'ManchesterOxfordRoad': {
-    people: 5211,
+    max_people: 5211,
     threshold: 4800,
     latitude: 53.47404,
     longitude: -2.24199
   },
   'Stockport': {
-    people: 3088,
+    max_people: 3088,
     threshold: 2800,
     latitude: 53.408619,
     longitude: -2.162632
   },
   'Bolton': {
-    people: 2771,
+    max_people: 2771,
     threshold: 2500,
     latitude: 53.577325,
     longitude: -2.432879
